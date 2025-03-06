eb916e371734708b73f5ede33e17cc8aae248b02
Hikmatulloh-sd <hikmatulloh.sd@gmail.com>
2025-03-06 14:27:42 +0600

    Create Task table and Register admin panel

---
+++ apps/tasks/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Task(models.Model):
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+        
+    title = models.CharField(
+        max_length=255,
+        verbose_name='Название задачи'
+    )
+    description = models.TextField(
+        verbose_name='Описание задачи'
+    )
+    status  = models.BooleanField(
+        default=False,
+        verbose_name='Статус задачи'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Последное обнавление'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создание'
+    )
+
+    def __str__(self):
+        return self.title
--- src/settings.py
+++ src/settings.py
@@ -28,6 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My apps
+    'apps.tasks.apps.TasksConfig'
 ]
 
 MIDDLEWARE = [
